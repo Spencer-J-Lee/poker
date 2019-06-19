f58f9d84fa2f5b4e0e0d67f5211714b266645fe2
Spencer <spenjlee@gmail.com>
2019-06-19 14:53:26 -0700

    Pass initialize and add_card tests

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,9 +1,12 @@
 class Hand
 	attr_reader :cards
-	
+
 	def initialize
+		@cards = []
 	end
 
 	def add_card(card)
+		raise ArgumentError "Hand already has 5 cards" if @cards.count >= 5
+		@cards << card
 	end
 end
