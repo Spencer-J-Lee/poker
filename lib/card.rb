9ea12de26b0e278654d87bf67accf2a079548aad
Spencer <spenjlee@gmail.com>
2019-06-19 14:07:17 -0700

    Add variables and tests for Card rank, suit, and value

--- lib/card.rb
+++ lib/card.rb
@@ -1,2 +1,30 @@
 class Card
+	attr_reader :rank, :suit, :value
+
+	VALUES = {
+		one:    1,
+		two:    2,
+		three:  3,
+		four:   4,
+		five:   5,
+		six:    6,
+		seven:  7,
+		eight:  8,
+		nine:   9,
+		ten:   10,
+		jack:  11,
+		queen: 12,
+		king:  13,
+		ace:   14
+	}
+
+	def initialize(rank, suit)
+		@rank  = rank
+		@suit  = suit
+		@value = VALUES[rank]
+	end
+
+	def inspect
+		"#{rank}_#{suit}"
+	end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,2 +1,18 @@
 require 'rspec'
 require 'card'
+
+describe Card do
+	subject(:card) { Card.new(:ace, :spades) }
+	
+	it "holds a rank" do
+		expect(card.rank).to eq(:ace)
+	end
+
+	it "holds a suit" do
+		expect(card.suit).to eq(:spades)
+	end
+
+	it "holds a value" do
+		expect(card.value).to eq(14)
+	end
+end
