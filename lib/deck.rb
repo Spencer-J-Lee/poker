c21e2288c331fee2a325bd469cf6013a5e20dba9
Spencer <spenjlee@gmail.com>
2019-06-19 14:44:51 -0700

    Implement shuffled_deck, draw, and reshuffle! methods for Deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,2 +1,27 @@
+require_relative 'card'
+
 class Deck
+	attr_reader :cards
+
+	def self.shuffled_deck
+		deck = []
+
+		Card.ranks.each do |rank|
+			Card.suits.each { |suit| deck << Card.new(rank, suit) }
+		end
+
+		deck.shuffle
+	end
+
+	def initialize
+		@cards = Deck.shuffled_deck
+	end
+
+	def draw
+		@cards.pop
+	end
+
+	def reshuffle!
+		@cards = Deck.shuffled_deck
+	end
 end
