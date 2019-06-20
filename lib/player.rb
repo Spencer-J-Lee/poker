6f4183cc14e23b7eef4b1558257b47998da5879b
Spencer <spenjlee@gmail.com>
2019-06-20 15:35:20 -0700

    Add card adder to Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,26 +1,44 @@
 class Player
 	attr_reader :hand, :pot
-	
+
 	def initialize
 		@hand = Hand.new
 		@pot  = 1000
+	end
+	
+	def add_card(card)
+		hand.add_card(card)
+	end
+
+	def fold
+
+	end
+	
+	def see
+
+	end
+	
+	def raise
+
+	end
+	
+	def discard
+
 	end
 end
 
 =begin
 
-Rules
+1. Each player has a hand, plus a pot
+2. Player has methods to ask the user:
+	3. Which cards they wish to discard
+	4. Whether they wish to fold, see, or raise.
+
 Each player is dealt five cards.
 Players bet; each player may fold, see the current bet, or raise.
 In turn, each player can choose to discard up to three cards.
 They are dealt new cards from the deck to replace their old cards.
 Players bet again.
 If any players do not fold, then players reveal their hands; strongest hand wins the pot.
-
-Player
-Each player has a hand, plus a pot
-Player has methods to ask the user:
-Which cards they wish to discard
-Whether they wish to fold, see, or raise.
 
 =end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,6 +3,7 @@ require 'player'
 
 describe Player do
 	subject(:player) { Player.new }
+	let(:card) { double("card", rank: 10, suit: :hearts, value: 10) }
 
 	describe "#initialize" do
 		it "creates a new, empty hand" do
@@ -11,6 +12,13 @@ describe Player do
 		
 		it "creates a new pot of 1000" do
 			expect(player.pot).to eq(1000)
+		end
+	end
+
+	describe "#add_card" do
+		it "adds a card to the player's hand" do
+			player.add_card(card)
+			expect(player.hand.cards).to include(card)
 		end
 	end
 end
