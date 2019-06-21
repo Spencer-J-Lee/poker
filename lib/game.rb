b23c5fd3e627fb16deb466158040b31d6aebf44e
Spencer <spenjlee@gmail.com>
2019-06-20 17:17:06 -0700

    Add player instance variables and tests

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,10 @@ require_relative 'player'
 require_relative 'deck'
 
 class Game
+	attr_reader :players, :current_player
+	
 	def initialize(*players)
+		@players = players
+		@current_player = players.first
 	end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,8 +7,16 @@ describe Game do
 	subject(:game) { Game.new(player1, player2)}
 
 	describe "#initialize" do
-		it "takes any number of Player classes as an argument" do
+		it "takes any number of Player instances as an argument" do
 			game
+		end
+
+		it "sets @players to an array of Player instances" do
+			expect(game.players).to eq([player1, player2])
+		end
+
+		it "keeps track of whose turn it is" do
+			expect(game.current_player).to eq(player1)
 		end
 	end
 end
